cfea430495cf3d23025240acbf87406d8bcdfec6
IsaacAndra <isaac.andra84@gmail.com>
2023-04-01 19:51:44 -0300

    :bulb: fix: token de acesso removido

--- src/reducer.js
+++ src/reducer.js
@@ -3,40 +3,71 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    //Remover o a string do token depois que terminar de desenvolver...
-    //token: 'BQC1WKxh3_Yw9-1cnTB-vXqfrzTghGHJunZ08Z_5hosHnOMESlkEO8ikWTRmFgunsVgFM2CbzAPlmFpetZqyeLQJ8NqZ_lNGISDTopT14Z5YZL8SViPCewCfEoE1AkPbZH8rwcR9_FNQqgW0aXx-xA7gYAUuLZT5N_QJhJKhOo0wsEiCvSGviZ7XESEg_-FAM_G0nWc',
 };
 
 const reducer = (state, action) => {
     console.log(action.discover_weekly);
     console.log(action);
-    switch(action.type) {
+    switch (action.type) {
         case 'SET_USER':
             return {
                 ...state,
                 user: action.user
             };
 
-            case 'SET_TOKEN':
-                return {
-                    ...state,
-                    token: action.token
-                };
-            
-            case 'SET_PLAYLISTS':
-                return {
-                    ...state,
-                    playlists: action.playlists,
-                };
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token
+            };
 
-            case 'SET_DISCOVER_WEEKLY':
-                return {
-                    ...state,
-                    discover_weekly: action.discover_weekly,
-                }
-                
-            default:
-                return state;
+        case 'SET_PLAYLISTS':
+            return {
+                ...state,
+                playlists: action.playlists,
+            };
+
+        case 'SET_DISCOVER_WEEKLY':
+            return {
+                ...state,
+                discover_weekly: action.discover_weekly,
+            }
+        case "SET_TOP_ARTISTS":
+            return {
+                ...state,
+                top_artists: action.top_artists,
+            };
+
+        case "SET_TOKEN":
+            return {
+                ...state,
+                token: action.token,
+            };
+
+        case "SET_SPOTIFY":
+            return {
+                ...state,
+                spotify: action.spotify,
+            };
+
+        case "SET_PLAYLISTS":
+            return {
+                ...state,
+                playlists: action.playlists,
+            };
+        case 'SET_PLAYING':
+            return {
+                ...state,
+                playing: action.playing,
+            }
+        case 'SET_ITEM':
+            return {
+                ...state,
+                item: action.item,
+            }
+
+        default:
+            return state;
     }
 
 }
